230e183afe89ab68bfa894a743a784419464eaa1
Dennis Pettersson <mail@dennispettersson.se>
2019-09-04 22:01:04 +0200

    fix: check if jwt-token exists

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -48,91 +48,104 @@ beforeEach(() => {
   dataStoreCreate = dataStore.create as jest.Mock
 
   user = { id: 'foo' }
-
-  isUserMock.mockReturnValue(true)
 })
 
 afterEach(() => {
   server.close()
 })
 
-describe('app routes', () => {
-  describe('GET /', () => {
-    it('gets a JSON on root-route', async () => {
-      const { body } = await supertest(app)
-        .get('/')
-        .expect(200)
+describe('/', () => {
+  it('gets a JSON on root-route', async () => {
+    const { body } = await supertest(app)
+      .get('/')
+      .expect(200)
 
-      expect(body).toEqual({
-        version: '13.37.0-beta-2-final',
-        name: 'test',
-      })
-    })
-  })
-
-  describe('GET /favicon', () => {
-    it('gets an empty 404 for /favicon.ico', async () => {
-      const { text } = await supertest(app)
-        .get('/favicon.ico')
-        .expect(404)
-
-      expect(text).toEqual('')
+    expect(body).toEqual({
+      version: '13.37.0-beta-2-final',
+      name: 'test',
     })
   })
 })
 
-describe('setup', () => {
-  describe('/group', () => {
-    it('parses the query-params', async () => {
-      const innerCallback = jest.fn()
-      dataStoreCreate.mockImplementation((_, callback) => {
-        innerCallback(callback(id))
-      })
+describe('/favicon.ico', () => {
+  it('gets an empty 404 for /favicon.ico', async () => {
+    const { text } = await supertest(app)
+      .get('/favicon.ico')
+      .expect(404)
 
-      const id = 'some generated uuid'
-      const defaultObject = {
-        id,
-        name: '',
-        startSum: 1000,
-        users: [user.id],
-      }
+    expect(text).toEqual('')
+  })
+})
 
-      const tests: (
-        | [string, Omit<CreateGroupInput, 'userID'>]
-        | [string, Omit<CreateGroupInput, 'userID'>, string])[] = [
-        ['/group', {}],
-        ['/group?', {}],
-        ['/group?name=foo', { name: 'foo' }],
-        ['/group?name=[fsffsf];;;', { name: '[fsffsf];;;' }],
-        ['/group?startSum=[fsffsf];;;', {}],
-        ['/group?startSum=1337', { startSum: 1337 }],
-        ['/group?startSum= 1337', { startSum: 1337 }],
-        ['/group?startSum=1337.1337', { startSum: 1337 }],
-        ['/group?startSum=1337.8', { startSum: 1337 }],
-        ['/group?startSum=1337,8', {}],
-        ['/group?startSum=-1337', { startSum: 0 }],
-        [
-          '/group?startSum=99999999999999999999999',
-          { startSum: Number.MAX_SAFE_INTEGER },
-          'set max-value',
-        ],
-      ]
+describe('/group', () => {
+  beforeEach(() => {
+    isUserMock.mockReturnValue(true)
+  })
 
-      for (const [query, result, message = ''] of tests) {
-        await supertest(app)
-          .get(query as string)
-          .expect(200)
+  it('requires a valid token', async () => {
+    isUserMock.mockReturnValue(false)
 
-        expect(dataStoreCreate, message).lastCalledWith(
-          dataStore.StoreTypes.Group,
-          expect.any(Function)
-        )
+    await supertest(app)
+      .get('/group')
+      .expect(401)
 
-        expect(innerCallback, message).lastCalledWith({
-          ...defaultObject,
-          ...result,
-        })
-      }
+    user = null
+    isUserMock.mockReturnValue(true)
+
+    await supertest(app)
+      .get('/group')
+      .expect(400)
+  })
+
+  it('parses the query-params', async () => {
+    const innerCallback = jest.fn()
+    dataStoreCreate.mockImplementation((_, callback) => {
+      innerCallback(callback(id))
     })
+
+    const id = 'some generated uuid'
+    const defaultObject = {
+      id,
+      name: '',
+      startSum: 1000,
+      users: [user.id],
+    }
+
+    const tests: (
+      | [string, Omit<CreateGroupInput, 'userID'>]
+      | [string, Omit<CreateGroupInput, 'userID'>, string])[] = [
+      ['/group', {}],
+      ['/group?', {}],
+      ['/group?name=foo', { name: 'foo' }],
+      ['/group?name=[fsffsf];;;', { name: '[fsffsf];;;' }],
+      ['/group?startSum=[fsffsf];;;', {}],
+      ['/group?startSum=1337', { startSum: 1337 }],
+      ['/group?startSum= 1337', { startSum: 1337 }],
+      ['/group?startSum=1337.1337', { startSum: 1337 }],
+      ['/group?startSum=1337.8', { startSum: 1337 }],
+      ['/group?startSum=1337,8', {}],
+      ['/group?startSum=-1337', { startSum: 0 }],
+      [
+        '/group?startSum=99999999999999999999999',
+        { startSum: Number.MAX_SAFE_INTEGER },
+        'set max-value',
+      ],
+    ]
+
+    for (const [query, result, message = ''] of tests) {
+      await supertest(app)
+        .get(query)
+        .expect(200)
+
+      expect(dataStoreCreate, message).lastCalledWith(
+        dataStore.StoreTypes.Group,
+        expect.any(Function)
+      )
+
+      expect(innerCallback, message).lastCalledWith({
+        ...defaultObject,
+        ...result,
+      })
+    }
   })
 })
